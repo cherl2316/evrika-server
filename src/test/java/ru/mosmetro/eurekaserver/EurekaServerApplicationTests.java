58ad1af1173e8f3a757fb987a86daf9635caec37
Alex <makeev-aa@mosmetro.ru>
2024-06-07 00:40:34 +0300

    Развёрнута эврика

---
+++ src/test/java/ru/mosmetro/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mosmetro.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
